e837c00a66191d6e020eb0d6e617bb2affa7a23d
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-12-02 15:48:21 -0500

    server set up

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
-console.log("HERE");
+const { response } = require("express");
+//required
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+
+//variables
+const app = express();
+const HTTP_PORT = process.env.PORT || 8080;
+
+//USE
+app.use(cors());
+app.use(express.json());
+
+
+function onHttpStart(){
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+app.listen(HTTP_PORT, onHttpStart);
