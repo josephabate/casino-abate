cbb556e315910e6d1a4f196d34fdd7bf36f3f072
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-12-04 16:38:16 -0500

    serialization for passports

---
+++ server/passportConfig.js
@@ -0,0 +1,35 @@
+const User = require('./User');
+const bcrypt = require('./bcruptjs');
+const { response } = require('express');
+const localStrategy = require('passport-local').Strategy;
+
+
+module.exports = (passport) => {
+    passport.use(
+        new localStrategy((email, password, done) => {
+            User.findOne({ email: email }, (err, user) => {
+                if (err) throw err;
+                else if (!user) return done(null, false);
+                else {
+                    bcrypt.compare(password, user.password, (err, result) => {
+                        if (err) throw err;
+                        else if (result) {
+                            return done(null, user);
+                        } else {
+                            return done(null, false);
+                        }
+                    })
+                }
+            })
+        })
+    )
+
+    passport.serializeUser((user,cb)=>{
+        cb(null, user.id);
+    })
+    passport.deserializeUser((id,cb) =>{
+        User.findOne({_id: id}, (err,user)=>{
+            cd(err, user);
+        })
+    })
+};
