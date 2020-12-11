ca2ae0878771d647e5ef140afd5af5cf3985a6aa
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-12-11 16:41:21 -0500

    added some game logic and payouts

--- client/src/components/SlotMachine/SlotMachine.js
+++ client/src/components/SlotMachine/SlotMachine.js
@@ -9,13 +9,14 @@ class SlotMachine extends Component {
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0]
         ],
-        interval: 0
+        interval: 0,
+        winners: []
     }
 
     randomNumber = (min, max) => {
         let number = Math.floor(Math.random() * (max - min)) + min;
-        let isWinner = false 
-        return {winner: isWinner, number: number}
+        let isWinner = false
+        return {isWinner: isWinner, number: number}
     }
 
     setSlotMachineNumbers = () => {
@@ -40,7 +41,7 @@ class SlotMachine extends Component {
     setNewSlot = () => {
         this.setSlotMachineNumbers();
 
-        if (this.state.interval < 25 ) {
+        if (this.state.interval < 10 ) {
             setTimeout(() => {
                 this.setNewSlot()
             }, 100);
@@ -48,6 +49,37 @@ class SlotMachine extends Component {
             this.setState({
                 interval: 0
             })
+
+            setTimeout(() => {
+                this.setNewSlot()
+            }, 1000);
+        }
+    }
+
+    /**     3           4           5           6
+     * 1 (0-120)    5           15          25          50
+     * 2 (0-240)    5           15          25          50
+     * 3 (0-360)    5           15          25          50
+     * 4 (0-480)    5           15          25          50
+     * 5 (0-600)    5           15          25          50
+     * 6 (0-720)    5           15          25          50
+     * 7 (0-120)    100.0       120.0       135.0       150.0
+     * 8 (0-120)    125.0       150.0       175.0       200.0
+     * 9 (0-120)    150.0       200.0       350.0       400.0
+     * 10 (0-120)   250.0       300.0       500.0       800.0
+     * 11 (0-120)   500.0       700.0       800.0       1000.0
+     * 
+     * 0 - 1000
+     * 
+     */
+    checkForWinner = () => {
+        let slot = this.state.slots;
+
+        let same = true;
+        for(let i = 0; i < slot.length; i++){
+            for(let x = 0; x < 2; x++){
+
+            }
         }
     }
 
@@ -65,7 +97,7 @@ class SlotMachine extends Component {
                                 {
                                     row.map((
                                         column) => {
-                                        return column.number
+                                        return <span key={uuidv4()} className={`${column.isWinner}`}>{column.number + " "}</span>
                                     }
                                     )
                                 }
