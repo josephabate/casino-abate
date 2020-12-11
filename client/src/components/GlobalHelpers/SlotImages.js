16f592795756d84ca2c1bbe2d34bcbeaef7a8d34
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-12-11 15:34:29 -0500

    added helper file for slot pictures and code

---
+++ client/src/components/GlobalHelpers/SlotImages.js
@@ -0,0 +1,27 @@
+import icon1 from '../../assets/images/slotIcons/slots-01.png'
+import icon2 from '../../assets/images/slotIcons/slots-02.png'
+import icon3 from '../../assets/images/slotIcons/slots-03.png'
+import icon4 from '../../assets/images/slotIcons/slots-04.png'
+import icon5 from '../../assets/images/slotIcons/slots-05.png'
+import icon6 from '../../assets/images/slotIcons/slots-06.png'
+import icon7 from '../../assets/images/slotIcons/slots-07.png'
+import icon8 from '../../assets/images/slotIcons/slots-08.png'
+import icon9 from '../../assets/images/slotIcons/slots-09.png'
+import icon10 from '../../assets/images/slotIcons/slots-10.png'
+import icon11 from '../../assets/images/slotIcons/slots-11.png'
+
+const slotIcons = [
+    icon1,
+    icon2,
+    icon3,
+    icon4,
+    icon5,
+    icon6,
+    icon7,
+    icon8,
+    icon9,
+    icon10,
+    icon11
+]
+
+export default slotIcons;
