3fbeea5870ef35baf28a932ed70285c310366d18
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-12-14 13:12:44 -0500

    added logout btn for front end

--- client/src/components/PlayerDashBoard/PlayerDashBoard.js
+++ client/src/components/PlayerDashBoard/PlayerDashBoard.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const PlayerDashBoard = ({username, money}) => {
     return (
         <div className="playerDashBoard">
-            <h2 className="playerDashBoard__title">{username}'s dashboard</h2>
+            <h2 className="playerDashBoard__title">{username}'s DASHBOARD</h2>
             <h2>Your money: ${money}</h2>
             <Link to="/">Back To Home</Link>
         </div>
