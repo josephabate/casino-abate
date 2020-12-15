c6fe23e13c8472b950ba870a22f86c047fc98c11
Joseph Abate <Joseph_Abate_@hotmail.com>
2020-12-15 12:54:26 -0500

    main registration styling

--- client/src/pages/Registration/Registration.js
+++ client/src/pages/Registration/Registration.js
@@ -3,14 +3,18 @@ import { Link } from 'react-router-dom';
 import SignIn from '../../components/SignIn/SignIn';
 import SignUp from '../../components/SignUp/SignUp';
 
+import './Registration.scss';
+
 class Registration extends Component {
 
     render() {
         return (
-            <div>
-                <SignIn/>
-                <Link to="/forget-password">Forget Password</Link>
-                <SignUp />                
+            <div className="Registration">
+                <div>
+                    <SignIn />
+                    <Link className="Registration__forget" to="/forget-password">Forget Password</Link>
+                </div>
+                <SignUp />
             </div>
         );
     }
